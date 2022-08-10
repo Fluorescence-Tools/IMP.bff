84d62e9ff0ac10783988461aba7c0b07cafedd40
tpeulen <thomas.peulen@posteo.de>
2022-08-10 10:12:00 +0200

    Add SIMD cmake

--- Setup.cmake
+++ Setup.cmake
@@ -2,3 +2,16 @@
 ###########################
 FIND_PACKAGE (mongoc-1.0 1.7 REQUIRED)
 LINK_LIBRARIES (mongo::mongoc_shared)
+
+# SIMD
+###########################
+SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
+INCLUDE(simd)
+IF(${AVX_FOUND})
+    MESSAGE("BUILD WITH SIMD")
+    IF (MSVC)
+        STRING(APPEND CMAKE_CXX_FLAGS " /arch:AVX")
+    ELSE(MSVC)
+        STRING(APPEND CMAKE_CXX_FLAGS " -Ofast -mavx -mfma -ffast-math -funroll-loops")
+    ENDIF (MSVC)
+ENDIF(${AVX_FOUND})
