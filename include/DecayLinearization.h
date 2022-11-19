d6fa0dfe61c05b1ef824e46ff9ee958a405fb8b2
tpeulen <thomas.peulen@posteo.de>
2022-11-19 22:12:20 +0100

    Fix

--- include/DecayLinearization.h
+++ include/DecayLinearization.h
@@ -24,6 +24,9 @@ IMPBFF_BEGIN_NAMESPACE
 
 class IMPBFFEXPORT DecayLinearization: public DecayModifier{
 
+private:
+
+    DecayCurve linearization_table_;
 
 public:
 
@@ -35,29 +38,49 @@ public:
         return get_data();
     }
 
-    void add(DecayCurve *decay) {
-        if ((decay != nullptr) && is_active()) {
+    /// Modify DecayCurve object and apply a linearization
+    void add(DecayCurve *out) override {
+        if ((out != nullptr) && is_active()) {
             DecayCurve *lt = get_linearization_table();
-            lt->resize(decay->size(), 1.0);
+            lt->resize(out->size(), 1.0);
             size_t start, stop;
-            start = std::min(get_start(), decay->size());
-            stop = std::min(get_stop(), decay->size());
+            start = std::min(get_start(out), out->size());
+            stop = std::min(get_stop(out), out->size());
             for (size_t i = start; i < stop; i++) {
-                decay->y[i] *= lt->y[i];
+                out->y[i] *= lt->y[i];
             }
         }
     }
 
+    /*!
+     * DecayLinearization is a DecayModifier that takes a DecayCurve as an input,
+     * computes for the input an (optionally) smoothed linearization_table.
+     *
+     * Applying DecayLinearization to a DecayCurve using DecayLinearization::add
+     * applies the linearization_table to the DecayCurve. The computation of a
+     * perturbed fluorescence decay model is a typical application of DecayLinearization.
+     */
     DecayLinearization(
             DecayCurve* linearization_table,
-            int start,
-            int stop,
-            bool active = false
-    ) : DecayModifier(linearization_table, start, stop, active){
+            int start, int stop,
+            bool active = true,
+            int n_window = 5
+    ) : DecayModifier(nullptr, start, stop, active){
 #if IMPBFF_VERBOSE
         std::clog << "DecayLinearization::DecayLinearization(double*, int, bool)" << std::endl;
 #endif
-    }
+        linearization_table_.set_x(linearization_table->get_x());
+        linearization_table_.set_y(linearization_table->get_y());
+        linearization_table_.apply_simple_moving_average(
+                get_start(linearization_table),
+                get_stop(linearization_table),
+                n_window,
+                true
+        );
+        set_linearization_table(&linearization_table_);
+        set_start(start);
+        set_stop(stop);
+}
 
 
 };
