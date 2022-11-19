4db99e4e27d8bf1c15974a2805d15d71f53dd471
tpeulen <thomas.peulen@posteo.de>
2022-11-19 22:45:28 +0100

    Fix CI

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,11 +2,11 @@
 git submodule update --recursive --init --remote
 rm -r -f build
 cd doc
+mkdir _build
 doxygen
 $PYTHON ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
 cd ..
 
-mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 ninja install -k 0
