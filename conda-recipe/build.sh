002034f47ab1fc9c31f5abb5e229fac555addc4d
tpeulen <thomas.peulen@posteo.de>
2023-10-25 22:19:10 +0200

    Remove mongo dependency
    IMP should not depend on libmonogo
    (moved to chinet repo)

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-git submodule update --recursive --init --remote
-cd doc
-mkdir -p _build/html/stable/api
-doxygen
-$PYTHON ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
-cd ..
 
 mkdir build
 cd build
@@ -14,4 +8,3 @@ ninja install -k 0 -j 4
 # Copy examples
 mkdir $PREFIX/share/doc/IMP/examples/bff
 cp -r ../examples/* $PREFIX/share/doc/IMP/examples/bff
-
