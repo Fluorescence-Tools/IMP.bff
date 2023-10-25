3783f9a223ff093c05cfdbed25457a0a742edc2f
tpeulen <thomas.peulen@posteo.de>
2023-10-25 10:50:07 +0200

    AVX issues

--- Setup.cmake
+++ Setup.cmake
@@ -6,19 +6,21 @@ LINK_LIBRARIES (mongo::mongoc_shared)
 
 # SIMD
 ###########################
-IF(WITH_AVX)
-    SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
-    INCLUDE(simd)
-    IF(${AVX_FOUND})
-        MESSAGE("BUILD WITH SIMD")
-        IF (MSVC)
-            # https://devblogs.microsoft.com/cppblog/simd-extension-to-c-openmp-in-visual-studio/
-            # /Oi is for intrinsics
-            SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:AVX -openmp:experimental /Oi")
-        ELSE(MSVC)
-            STRING(APPEND CMAKE_CXX_FLAGS " -Ofast -mavx -mfma -ffast-math -funroll-loops")
-        ENDIF (MSVC)
-    ELSE ()
-        UNSET(WITH_AVX)
-    ENDIF(${AVX_FOUND})
-ENDIF(WITH_AVX)
+IF(NOT APPLE)
+    IF(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+        SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
+        INCLUDE(simd)
+        IF(${AVX_FOUND})
+            MESSAGE("BUILD WITH SIMD")
+            IF (MSVC)
+                # https://devblogs.microsoft.com/cppblog/simd-extension-to-c-openmp-in-visual-studio/
+                # /Oi is for intrinsics
+                SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:AVX -openmp:experimental /Oi")
+            ELSE(MSVC)
+                STRING(APPEND CMAKE_CXX_FLAGS " -Ofast -mavx -mfma -ffast-math -funroll-loops")
+            ENDIF (MSVC)
+        ELSE(${AVX_FOUND})
+            UNSET(WITH_AVX)
+        ENDIF(${AVX_FOUND})
+    ENDIF()
+ENDIF()
