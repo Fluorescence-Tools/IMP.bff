e002f5f2c268dd207a62534383987b3d6706e8ac
tpeulen <thomas.peulen@posteo.de>
2023-10-25 08:31:15 +0200

    Turn on AVX by default

--- Setup.cmake
+++ Setup.cmake
@@ -5,13 +5,17 @@ LINK_LIBRARIES (mongo::mongoc_shared)
 
 # SIMD
 ###########################
-SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
-INCLUDE(simd)
-IF(${AVX_FOUND} AND NOT APPLE)
-    MESSAGE("BUILD WITH SIMD")
-    IF (MSVC)
-        STRING(APPEND CMAKE_CXX_FLAGS " /arch:AVX")
-    ELSE(MSVC)
-        STRING(APPEND CMAKE_CXX_FLAGS " -Ofast -mavx -mfma -ffast-math -funroll-loops")
-    ENDIF (MSVC)
-ENDIF(${AVX_FOUND} AND NOT APPLE)
+IF(WITH_AVX)
+    SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
+    INCLUDE(simd)
+    IF(${AVX_FOUND})
+        MESSAGE("BUILD WITH SIMD")
+        IF (MSVC)
+            # https://devblogs.microsoft.com/cppblog/simd-extension-to-c-openmp-in-visual-studio/
+            # /Oi is for intrinsics
+            SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:AVX -openmp:experimental /Oi")
+        ELSE(MSVC)
+            STRING(APPEND CMAKE_CXX_FLAGS " -Ofast -mavx -mfma -ffast-math -funroll-loops")
+        ENDIF (MSVC)
+    ENDIF(${AVX_FOUND})
+ENDIF(WITH_AVX)
