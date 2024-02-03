5348c471c7bd4c16856000e0d243b3355fc6c567
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-02-03 18:19:23 +0100

    Clean up and add AVX option

--- Setup.cmake
+++ Setup.cmake
@@ -1,21 +1,27 @@
 # SIMD
 ###########################
-IF(NOT APPLE)
-    IF(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
-        SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
-        INCLUDE(simd)
-        IF(${AVX_FOUND})
-            MESSAGE("BUILD WITH SIMD")
-            ADD_DEFINITIONS(-DWITH_AVX)
-            IF (MSVC)
-                # https://devblogs.microsoft.com/cppblog/simd-extension-to-c-openmp-in-visual-studio/
-                # /Oi is for intrinsics
-                SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:AVX /Oi")
-            ELSE(MSVC)
-                SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mavx -mfma")
-            ENDIF (MSVC)
-        ELSE(${AVX_FOUND})
-            UNSET(WITH_AVX)
-        ENDIF(${AVX_FOUND})
-    ENDIF()
-ENDIF()
+
+option(WITH_AVX "Enable AVX support" OFF)
+
+if (WITH_AVX AND NOT APPLE AND CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
+    include(simd)
+
+    if (${AVX_FOUND})
+        message("BUILD WITH SIMD")
+        add_definitions(-DWITH_AVX)
+
+        if (MSVC)
+            # https://devblogs.microsoft.com/cppblog/simd-extension-to-c-openmp-in-visual-studio/
+            # /Oi is for intrinsics
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:AVX /Oi")
+        else (MSVC)
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mavx -mfma")
+        endif (MSVC)
+    else (${AVX_FOUND})
+        unset(WITH_AVX)
+    endif (${AVX_FOUND})
+else ()
+    # AVX only on x86_64 and not on Apple
+    unset(WITH_AVX)
+endif()
